219deeac17481aaf103bfb818bc065e297d016d2
calixtorafael-1 <calixxtorafaell@gmail.com>
2023-10-28 20:12:21 -0300

    update the index file

--- projeto_api/index.js
+++ projeto_api/index.js
@@ -1,83 +1,184 @@
 const express = require('express');
 const fs = require('fs');
-const app = express(); 
-const port = 3000; // define o valor para uma porta
+const axios = require('axios'); 
+const app = express();
+const port = 3000;
 
+app.use(express.json());
 
-app.use(express.json()); //permite utilizar json no body das requisições
-
-
-// Função para puxar dados do db.json
-function fetchData() {
-    const rawData = fs.readFileSync('db.json');
-    return JSON.parse(rawData);
+//Funcao para ler os dados de 'db.funcionarios.json'
+function fetchDataFuncionarios() {
+    try {
+        const rawData = fs.readFileSync('db.funcionarios.json', 'utf8');
+        return JSON.parse(rawData);
+    } catch (err) {
+        return { funcionarios: [] };
+    }
 }
-function saveData(data){
-    const jsonData = JSON.stringify(data, null, 2);
-    fs.writeFileSync('db.json', jsonData);
-} 
 
-// Rota GET para obter dados
-app.get('/api/dados', (req, res) => {
-    const dados = fetchData();
-    res.json(dados);
+// Funcao para ler os dados de 'db.empresas.json'
+function fetchDataEmpresas() {
+    try {
+        const rawData = fs.readFileSync('db.empresas.json', 'utf8');
+        return JSON.parse(rawData);
+    } catch (err) {
+        return { empresas: [] };
+    }
+}
+
+// Funcao para salvar os dados de 'db.funcionarios.json'
+function saveDataFuncionarios(data) {
+    const jsonData = JSON.stringify(data, null, 2);
+    fs.writeFileSync('db.funcionarios.json', jsonData, 'utf8');
+}
+// Funcao para ler os dados de 'db.empresas.json'
+function saveDataEmpresas(data) {
+    const jsonData = JSON.stringify(data, null, 2);
+    fs.writeFileSync('db.empresas.json', jsonData, 'utf8');
+}
+
+// Rota para obter uma lista com todos os funcionarios
+app.get('/api/funcionarios', (req, res) => {
+    const funcionarios = fetchDataFuncionarios().funcionarios;
+    res.json(funcionarios);
 });
 
-
-//API RestFul
-
-//Busca por ID
-app.get('/api/dados/:id', (req, res) => {
-    const dados = fetchData();
-    const id = req.params.id;
-    const dado = dados.find(item => item.id === parseInt(id));
-    if (dado) {
-        res.json(dado);
+// Rota para obter um funcionario especifico atraves do ID
+app.get('/api/funcionarios/:idfuncionario', (req, res) => {
+    const dados = fetchDataFuncionarios().funcionarios;
+    const id = req.params.idfuncionario;
+    const funcionario = dados.find(item => item.id === parseInt(id));
+    if (funcionario) {
+        res.json(funcionario);
     } else {
-        res.status(404).send('Dado não encontrado');
+        res.status(404).send('Funcionario não encontrado');
     }
 });
-// Rota POST para adicionar novo dado
-app.post('/api/dados', express.json(), (req, res) => {
-    const dados = fetchData();
+
+// Rota para obter informacoes do endereco do funcionario atraves do ID e ddde uma consulta externa no VIACEP
+app.get('/api/funcionarios/:idfuncionario/endereco', async (req, res) => {
+    try {
+        const dados = fetchDataFuncionarios().funcionarios;
+        const id = req.params.idfuncionario;
+        const funcionario = dados.find(item => item.id === parseInt(id));
+
+        if (!funcionario || !funcionario.cep) {
+            res.status(404).send('Funcionario não encontrado ou não possui CEP.');
+            return;
+        }
+
+        // Faz uma requisicao ao viacep.com usando o cep do funcionario 
+        const cep = funcionario.cep;
+        const viacepResponse = await axios.get(`http://viacep.com.br/ws/${cep}/json/`);
+        res.json(viacepResponse.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao buscar o endereço do funcionario.');
+    }
+});
+
+// rota para adicionar um novo funcionario
+app.post('/api/funcionarios', express.json(), (req, res) => {
+    const dados = fetchDataFuncionarios();
     const novoDado = req.body;
-    dados.push(novoDado);
-    saveData(dados);
+    const funcionarios = dados.funcionarios;
+    novoDado.id = funcionarios.length > 0 ? Math.max(...funcionarios.map(f => f.id)) + 1 : 1;
+    funcionarios.push(novoDado);
+    saveDataFuncionarios(dados);
     res.json(novoDado);
 });
 
-// Rota PUT para atualizar dado existente por ID
-app.put('/api/dados/:id', express.json(), (req, res) => {
-    const dados = fetchData();
+// Atualizar funcionarios atraves de seu ID
+app.put('/api/funcionarios/:id', express.json(), (req, res) => {
+    const dados = fetchDataFuncionarios();
     const id = req.params.id;
-    const index = dados.findIndex(item => item.id === parseInt(id));
+    const funcionarios = dados.funcionarios;
+    const index = funcionarios.findIndex(item => item.id === parseInt(id));
 
     if (index !== -1) {
-        dados[index] = req.body;
-        saveData(dados);
-        res.json(dados[index]);
+        funcionarios[index] = req.body;
+        saveDataFuncionarios(dados);
+        res.json(funcionarios[index]);
     } else {
-        res.status(404).send('Dado não encontrado');
+        res.status(404).send('Funcionario não encontrado');
+    }
+    
+});
+
+//############### EMPRESAS ###################
+
+//obter empresas lista de empresas
+app.get('/api/empresas', (req, res) => {
+    const empresas = fetchDataEmpresas().empresas;
+    res.json(empresas);
+});
+
+//Obter uma unica empresa
+app.get('/api/empresas/:idempresa', (req, res) => {
+    const dados = fetchDataEmpresas().empresas;
+    const id = req.params.idempresa;
+    const empresa = dados.find(item => item.id === parseInt(id));
+    if (empresa) {
+        res.json(empresa);
+    } else {
+        res.status(404).send('Empresa não encontrada');
     }
 });
 
-// Rota DELETE para excluir dado por ID
-app.delete('/api/dados/:id', (req, res) => {
-    const dados = fetchData();
-    const id = req.params.id;
-    const index = dados.findIndex(item => item.id === parseInt(id));
 
-    if (index !== -1) {
-        const deletedData = dados.splice(index, 1);
-        saveData(dados);
-        res.json(deletedData[0]);
+//obter funcionarios vinculados a empresa 
+app.get('/api/empresas/:idEmpresa/funcionarios', (req, res) => {
+    const idEmpresa = req.params.idEmpresa;
+    const funcionarios = fetchDataFuncionarios().funcionarios;
+    
+    // filtrar funcionarios por idEmpresa
+    const funcionariosDaEmpresa = funcionarios.filter(funcionario => funcionario.idEmpresa == idEmpresa);
+
+    if (funcionariosDaEmpresa.length > 0) {
+        res.json(funcionariosDaEmpresa);
     } else {
-        res.status(404).send('Dado não encontrado');
+        res.status(404).send('Nenhum funcionario encontrado para esta empresa.');
     }
 });
 
 
-// Inicia o servidor na porta especificada
+// CNPJ JA
+
+
+const apiKey = 'e0a9cc03-3b9a-47f8-af1c-886b472dd1bd-b7d6e4a0-cb4f-41a1-a51d-14c277d58ace';
+
+// Rota para obter a informacao sobre uma empresa através do ID
+app.get('/api/empresas/:idempresa/informacoes', async (req, res) => {
+    try {
+        const dados = fetchDataEmpresas().empresas;
+        const id = req.params.idempresa;
+        const empresa = dados.find(item => item.id === parseInt(id));
+
+        if (!empresa || !empresa.cnpj) {
+            res.status(404).send('Empresa não encontrada ou não possui CNPJ.');
+            return;
+        }
+
+        // Fazer uma requisicao ao CNPJ J zusando o CNPJ da empresa
+        const cnpj = empresa.cnpj;
+        const config = {
+            method: 'get',
+            url: `https://api.cnpja.com/office/${cnpj}`,
+            headers: {
+                'Authorization': apiKey
+            }
+        };
+
+        const cnpjResponse = await axios(config);
+        res.json(cnpjResponse.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao buscar informações da empresa.');
+    }
+});
+
+
+// ligar o servidor
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${3000}`);
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
